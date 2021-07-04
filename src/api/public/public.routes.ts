6c3ebe7ad927baaef9fbf8fae68ca948994eed2c
Ravi Deevan <ravideevan3@gmail.com>
2021-07-05 04:43:36 +0800

    add public get files

--- src/api/public/public.routes.ts
+++ src/api/public/public.routes.ts
@@ -1,8 +1,17 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 import TerritoryRoutes from './territory/territory.routes';
+import setting from '@src/../setting';
 
 const router = Router();
+
+/**
+ * GET /api/public/file/:filename
+ * get a file from backend
+ */
+router.get('/file/:filename*', (req: Request, res: Response) => {
+	res.sendFile(setting.PROJECT_DIR + '/public' + req.params['0']);
+});
 
 // route "{url}/api/public/territories"
 router.use('/territories', TerritoryRoutes);
